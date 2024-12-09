ed33eefe20012e3a16bad59d538a7da5b6884755
Treach <ingo.schmid96@yahoo.de>
2024-12-09 02:09:22 +0100

    new order main method csvHandler

--- handlers/csvHandler.py
+++ handlers/csvHandler.py
@@ -4,6 +4,47 @@ from handlers.abstractHandler import Handler
 
 
 class csvHandler(Handler):
+    def __init__(self):
+        pass
+
+    def useFile(self, filePath: str, modus: str):
+        super().useFile(filePath, modus)
+
+    def modusHandler(self, modus: str, filePath: str):
+        super().modusHandler(modus, filePath)
+
+    def read(self, filePath: str):
+        file = open(filePath, 'r')
+        content = file.read()
+        dataArr: [] = self.createDataArray(content)
+        super().printContentWithLineNumber(dataArr)
+        print(dataArr)
+        file.close()
+
+    def readAndWrite(self, filePath: str, modus: str):
+        choice = self.askUserForChoice()
+        match choice:
+            case "change":
+                print("changes will be made")
+                file = open(filePath, modus)
+                content: str = file.read()
+                dataArr: [] = self.createDataArray(content)
+                super().printContentWithLineNumber(dataArr)
+                number: int = self.askForLineNumber(dataArr)
+                lineToAdjust: [] = self.getLineFromArray(dataArr, number)
+                lineDict: dict = self.convertToDict(lineToAdjust, dataArr)
+                updatedDict: dict = self.askForKeyAndUpdateDict(lineDict)
+                dataArr[number]: str = self.convertBackToString(updatedDict)
+                self.printContentWithLineNumber(dataArr)
+                print("updated:", updatedDict)
+                self.saveToFile(file, dataArr)
+            case "append":
+                file = open(filePath, 'a')
+                self.appendToFile(file)
+                file.close()
+
+    def writeToFile(self, filePath: str, modus: str):
+        pass
 
     def appendToFile(self, file):
         super().appendToFile(file)
@@ -58,45 +99,3 @@ class csvHandler(Handler):
     def createDataArray(self, content: str):
         dataArray: [] = content.splitlines()
         return dataArray
-
-    def useFile(self, filePath: str, modus: str):
-        super().useFile(filePath, modus)
-
-    def modusHandler(self, modus: str, filePath: str):
-        super().modusHandler(modus, filePath)
-
-    def read(self, filePath: str):
-        file = open(filePath, 'r')
-        content = file.read()
-        dataArr: [] = self.createDataArray(content)
-        super().printContentWithLineNumber(dataArr)
-        print(dataArr)
-        file.close()
-
-    def readAndWrite(self, filePath: str, modus: str):
-        choice = self.askUserForChoice()
-        match choice:
-            case "change":
-                print("changes will be made")
-                file = open(filePath, modus)
-                content: str = file.read()
-                dataArr: [] = self.createDataArray(content)
-                super().printContentWithLineNumber(dataArr)
-                number: int = self.askForLineNumber(dataArr)
-                lineToAdjust: [] = self.getLineFromArray(dataArr, number)
-                lineDict: dict = self.convertToDict(lineToAdjust, dataArr)
-                updatedDict: dict = self.askForKeyAndUpdateDict(lineDict)
-                dataArr[number]: str = self.convertBackToString(updatedDict)
-                self.printContentWithLineNumber(dataArr)
-                print("updated:", updatedDict)
-                self.saveToFile(file, dataArr)
-            case "append":
-                file = open(filePath, 'a')
-                self.appendToFile(file)
-                file.close()
-
-    def writeToFile(self, filePath: str, modus: str):
-        pass
-
-    def __init__(self):
-        pass
